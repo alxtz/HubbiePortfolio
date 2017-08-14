d2c31536167aa4af379883cc72e74605891200cc
Alex Tzeng <alxtz.tw@gmail.com>
2017-08-14 16:15:35 +0800

    gh-pages

---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="root">
+
+    <div class="payload">
+
+      <h1 class="hubbie">HUBBIE</h1>
+
+      <div class="animateBox">
+        <input class="codeBox" placeholder="Please Enter Your Code" type="text">
+      </div>
+
+      <ul class="skillList">
+        <li class="skill">Product Design</li>
+        <li class="skill">Motion Graphic</li>
+        <li class="skill">UI</li>
+        <li class="skill">Photography</li>
+      </ul>
+
+      <div class="enter">ENTER</div>
+
+    </div>
+
+    <div class="background">
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data () {
+      return {
+        msg: 'Welcome to the home page.'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @import "~@/scss/components/Home";
+</style>
